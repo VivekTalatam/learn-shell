3f47ac686288f7065a7b9463a2d1036edd77d3b8
Vivek Talatam <vivektalatam2000@gmail.com>
2026-03-01 20:24:58 +0530

    changes made

--- variables.sh
+++ variables.sh
@@ -4,5 +4,7 @@ z=$1
 echo $z  #if we give the var in echo it will check for LHS only
 
 
-DATE=$(date)
-echo Today:- $DATE 
+DATE=$(date) #No spaces while declaring variables.. :) 
+echo Today:- $DATE
+Addition=$(($1+$2))
+echo Addition of $1+$2 is $Addition

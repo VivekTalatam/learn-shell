dcc997a9e6a99e7df16497817979feff027fae6c
Vivek Talatam <vivektalatam2000@gmail.com>
2026-03-01 20:32:31 +0530

    addding new file

---
+++ functions.sh
@@ -0,0 +1,9 @@
+# syntax
+#  function_name(){
+#      commands                 
+# }
+# Accessing the function
+example(){
+    echo printing the command from example function, Date:- $(date)
+}
+example
